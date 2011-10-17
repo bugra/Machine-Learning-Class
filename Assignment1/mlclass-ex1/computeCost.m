ba3a6f12842016ddf58a997c2c238c210853db59
Bugra <bugraa@live.com>
2011-10-16 22:57:08 -0400

    Homework Main Part 1 Completed

--- Assignment1/mlclass-ex1/computeCost.m
+++ Assignment1/mlclass-ex1/computeCost.m
@@ -8,7 +8,7 @@ m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
 J = 0;
-
+J = (1/(2*m))*sum(((X*theta)-y).^2);
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
--- Assignment1/mlclass-ex1/gradientDescent.m
+++ Assignment1/mlclass-ex1/gradientDescent.m
@@ -7,7 +7,8 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
-for iter = 1:num_iters
+for iter = 1:num_iters	
+
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
@@ -16,11 +17,10 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
+ squareRootCost0 = (1/m)*sum(((X*theta)-y).*X(:,1));
+ squareRootCost1 = (1/m)*sum(((X*theta)-y).*X(:,2));
+theta(1) = theta(1) - (alpha*squareRootCost0);
+theta(2) = theta(2) - (alpha*squareRootCost1);
 
 
     % ============================================================
