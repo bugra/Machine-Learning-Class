5cb7a00f9500ddee1d20b9faa9de33b8a6df3390
Bugra <bugraa@live.com>
2011-10-17 00:38:01 -0400

    Multi Variable Cost Computation Completed

--- Assignment1/mlclass-ex1/gradientDescent.m
+++ Assignment1/mlclass-ex1/gradientDescent.m
@@ -17,10 +17,14 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
- squareRootCost0 = (1/m)*sum(((X*theta)-y).*X(:,1));
+  squareRootCost0 = (1/m)*sum(((X*theta)-y).*X(:,1));
  squareRootCost1 = (1/m)*sum(((X*theta)-y).*X(:,2));
 theta(1) = theta(1) - (alpha*squareRootCost0);
 theta(2) = theta(2) - (alpha*squareRootCost1);
+
+
+
+
 
 
     % ============================================================
