3307a0ae196ef4a38f7062b76ec99c3142a5e423
Bugra <bugraa@live.com>
2011-10-29 14:33:53 -0400

    Second Assignment 2

--- Assignment2/mlclass-ex2/sigmoid.m
+++ Assignment2/mlclass-ex2/sigmoid.m
@@ -8,8 +8,10 @@ g = zeros(size(z));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
-
-
+rowLength = size(z,1);
+columnLength = size(z,2);
+ii = 1: (rowLength*columnLength);
+g(ii) = 1 / (1 + exp(-z(ii)));
 
 
 
