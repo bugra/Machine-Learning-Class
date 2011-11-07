dc07b2fb8ecdea7a74cf4a953cff05a72c35e37c
Bugra <bugraa@live.com>
2011-11-06 20:09:05 -0500

    PredictOnevsAll is completed, part 3 finished

--- Assignment3/mlclass-ex3/ex3.m
+++ Assignment3/mlclass-ex3/ex3.m
@@ -62,7 +62,7 @@ pause;
 
 
 %% ================ Part 3: Predict for One-Vs-All ================
-%  After ...
+%  After ...	
 pred = predictOneVsAll(all_theta, X);
 
 fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
--- Assignment3/mlclass-ex3/predictOneVsAll.m
+++ Assignment3/mlclass-ex3/predictOneVsAll.m
@@ -29,7 +29,8 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
-temp = 
+temp = X*(all_theta)';
+[temp p] = max(temp,[],2);
 
 
 
