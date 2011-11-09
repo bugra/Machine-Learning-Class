5d4042893e45ae32ced9fc02cdda9f134b1a5d50
Bugra <bugraa@live.com>
2011-11-09 14:09:07 -0500

    Regularized Gradient (Backpropagation) and Assignment 4 completed

--- Assignment4/mlclass-ex4/nnCostFunction.m
+++ Assignment4/mlclass-ex4/nnCostFunction.m
@@ -115,8 +115,10 @@ Theta2_grad = delta3' * (a2);
 Theta1_grad = (1/m) .* Theta1_grad;
 Theta2_grad = (1/m) .* Theta2_grad;
 
-
-
+Theta1_grad = Theta1_grad + ((lambda/(m)).*Theta1);
+Theta2_grad = Theta2_grad + ((lambda/(m)).*Theta2);
+Theta1_grad(:,1) = Theta1_grad(:,1) -  ((lambda/(m)).*Theta1(:,1));
+Theta2_grad(:,1) = Theta2_grad(:,1) -  ((lambda/(m)).*Theta2(:,1));
 end
 
 
