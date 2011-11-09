39e2d372812b108312d20d1a314db878c16ee561
Bugra <bugraa@live.com>
2011-11-08 21:10:31 -0500

    Gradient of Sigmoid is completed

--- Assignment4/mlclass-ex4/nnCostFunction.m
+++ Assignment4/mlclass-ex4/nnCostFunction.m
@@ -61,7 +61,19 @@ Theta2_grad = zeros(size(Theta2));
 %               the regularization separately and then add them to Theta1_grad
 %               and Theta2_grad from Part 2.
 %
-
+hx = sigmoid(X*theta);
+regTheta = theta(2:end);
+regularizationParameter = ((lambda/(2*m))*sum(regTheta.*regTheta));
+J = (-1/(m))* sum( y .* log(hx) + (1 - y) .* log(1 - hx) ) + regularizationParameter;
+% Gradient Operator
+for jj = 1:size(X,2)
+       grad(jj) = (1/m)*sum((hx-y).*X(:,jj)) ;
+end
+for kk = 1:size(X,2)
+   regularizationFactor = (1/m)*(lambda.*theta);
+end
+regularizationFactor(1) = 0;
+grad = grad + regularizationFactor;
 
 
 
